835e42a28b41f909acfceaac6565e5feece86e59
Kokorin-Danil <kokorindanil474@gmail.com>
2024-11-22 09:55:34 +0400

    добавил авторизацию gittoken

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,12 +1,16 @@
 import { Bot } from "grammy";
 import { authCommand } from "./commands/authCommand.js";
 import { config } from "../utils/config.js";
+import { tokenHandler } from "./commands/tokenHandler.js";
 
 // Создаем бота
 const bot = new Bot(config.BOT_API_KEY);
 
-// Регистрация команды /auth
-bot.command("auth", authCommand);
+const chatTokens = new Map();
+
+// Регистрация команд
+bot.command("auth", (ctx) => authCommand(ctx, chatTokens));
+bot.on("message:text", (ctx) => tokenHandler(ctx, chatTokens));
 
 // Запуск бота
 bot.start();
--- src/bot/commands/authCommand.js
+++ src/bot/commands/authCommand.js
@@ -1,15 +1,16 @@
 import { InlineKeyboard } from "grammy";
 
-export function authCommand(ctx) {
-  // Создаем клавиатуру с кнопкой для авторизации
+export function authCommand(ctx, chatTokens) {
+  // Очистить возможный ранее сохраненный токен для текущего чата
+  chatTokens.delete(ctx.chat.id);
+
   const keyboard = new InlineKeyboard().url(
-    "Авторизоваться через GitHub",
+    "Создать токен GitHub",
     "https://github.com/settings/tokens" // Ссылка для создания токена
   );
 
-  // Отправляем сообщение с кнопкой
   ctx.reply(
-    "Для авторизации создайте персональный токен GitHub и отправьте его мне. Нажмите на кнопку ниже, чтобы создать токен.",
+    "Для авторизации введите персональный токен GitHub. Нажмите на кнопку ниже, чтобы создать токен.",
     { reply_markup: keyboard }
   );
 }
